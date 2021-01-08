074efb884b6c105ca83cae9268a46b3e36cc1842
Kayu <klui69308@gmail.com>
2021-01-07 22:28:44 -0600

    first commit

---
+++ Practice/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice.Models;
+
+namespace Practice.Controllers
+{
+    public class MovieController : Controller
+    {
+        List<Movie> Collection1 = new List<Movie>()
+        {
+            new Movie{ MovieID = 1, Name = "Hello World", Genre = "Action", Price = 100},
+            new Movie{ MovieID = 2, Name = "Goodbye World", Genre = "Action", Price = 100}
+        };
+        // GET: Movie
+        public PartialViewResult DisplayMovieCollection()
+        {
+            return PartialView();
+        }
+        public ActionResult Index()
+        {
+            return View(Collection1);
+        }
+    }
+}
---
+++ Practice/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practice.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Genre must be specified")]
+        public string Genre { get; set; }
+        [Range(1, 1000, ErrorMessage = "Price must be between $1 and $1000")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double Price { get; set; }
+    }
+}
